4d50798998fb6cd02abb7de9132e747796398fb9
Adel Atallah <2213999+atallahade@users.noreply.github.com>
2018-10-13 13:04:50 +0200

    Improve the install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-PATH="$(dirname "$(which "$0")")"
+#!/bin/bash
+PATH="$(dirname $(readlink -f $0))"
 /bin/ln -sv "$PATH/vim/vimrc" ~/.vimrc
 /bin/ln -Tsv "$PATH/vim/vim" ~/.vim
 /bin/ln -sv "$PATH/tmux/.tmux.conf" ~/.tmux.conf
