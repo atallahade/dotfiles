d4711625a707d8c5b63ac3221c2285e5af630001
Adel Atallah <2213999+atallahade@users.noreply.github.com>
2019-02-24 21:39:41 +0000

    Add git config

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 PATH="$(dirname $(readlink -f $0))"
 /bin/ln -sv "$PATH/vim/vimrc" ~/.vimrc
 /bin/ln -Tsv "$PATH/vim/vim" ~/.vim
-/bin/ln -sv "$PATH/tmux/.tmux.conf" ~/.tmux.conf
+/bin/ln -sv "$PATH/tmux/tmux.conf" ~/.tmux.conf
+/bin/ln -sv "$PATH/git/gitconfig" ~/.gitconfig
