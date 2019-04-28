44f0093cb677dcd226c7e3662e502b1801c44a84
Adel Atallah <2213999+atallahade@users.noreply.github.com>
2019-04-28 11:06:10 +0100

    Add fish config and improve vimrc and install script

--- install.sh
+++ install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-PATH="$(dirname $(readlink -f $0))"
-/bin/ln -sv "$PATH/vim/vimrc" ~/.vimrc
-/bin/ln -Tsv "$PATH/vim/vim" ~/.vim
-/bin/ln -sv "$PATH/tmux/tmux.conf" ~/.tmux.conf
-/bin/ln -sv "$PATH/git/gitconfig" ~/.gitconfig
+DIR=pwd
+cd $(dirname $(readlink -f $0))
+/bin/ln -Tsv $(pwd)/vim/vim ~/.vim
+/bin/ln -sv $(pwd)/vim/vimrc ~/.vimrc
+/bin/ln -sv $(pwd)/tmux/tmux.conf ~/.tmux.conf
+/bin/ln -sv $(pwd)/git/gitconfig ~/.gitconfig
+cd fish
+for file in $(find . -type f)
+do
+  mkdir -p $(dirname ~/.config/fish/$file)
+  /bin/ln -sv $(pwd)/$file  ~/.config/fish/$(dirname $file)
+done
+cd ..
+cd $DIR
